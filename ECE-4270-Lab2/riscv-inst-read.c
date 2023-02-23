2b505e33a2f591b699dc7861f878a803fd832de7
cameron-ellis <cameronellis46@gmail.com>
2023-02-23 17:50:26 -0600

    add riscv-inst-read c and header file to read riscv assembly and to send to inst-to-hex

---
+++ ECE-4270-Lab2/riscv-inst-read.c
@@ -0,0 +1,7 @@
+#include "riscv-inst-read.h"
+
+struct inst inst_read(const char* filepath) {
+    FILE *f = fopen(filepath, "r");
+
+    
+}
---
+++ ECE-4270-Lab2/riscv-inst-read.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct inst {
+    char type;
+    void* type_block;
+} inst;
+
+typedef struct r_type {
+    char inst_name[7];
+    int opcode;
+    int f3;
+    int f7;
+    int rd;
+    int rs1;
+    int rs2;
+} r_type;
+
+typedef struct i_type {
+    char inst_name[7];
+    int opcode;
+    int f3;
+    int imm;
+    int rd;
+    int rs1;
+} i_type;
+
+typedef struct s_type {
+    char inst_name[7];
+    int opcode;
+    int f3;
+    int rd;
+    int rs1;
+    int imm;
+} s_type;
+
+typedef struct b_type {
+    char inst_name[7];
+    int opcode;
+    int f3;
+    int rs1;
+    int rs2;
+    int imm;
+} b_type;
+
+typedef struct j_type {
+    char inst_name[7];
+    int opcode;
+    int rd;
+    int imm;
+} j_type;
+
+struct inst inst_read(char* filepath);
