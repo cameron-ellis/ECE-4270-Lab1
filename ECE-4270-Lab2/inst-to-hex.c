6db1568d4a086a962155c20741c0a69d9f8453b2
ashbtp <ashbtp@cengr-c1251-114.engineering.missouri.edu>
2023-03-02 18:01:44 -0600

    Finished inst-to-hex file and added fibonnaci assembly code file

--- ECE-4270-Lab2/inst-to-hex.c
+++ ECE-4270-Lab2/inst-to-hex.c
@@ -1,32 +1,7 @@
-typedef struct r_type{
-    char inst_name[7];
-    int opcode, f3, f7, rd, rs1, rs2;
-} r_type;
-
-typedef struct i_type{
-    char inst_name[7];
-    int opcode, f3, imm, rd, rs1;
-} i_type;
-
-typedef struct s_type{
-    char inst_name[7];
-    int opcode, f3, imm, rd, rs1;
-} s_type;
-
-typedef struct b_type{
-    char inst_name[7];
-    int opcode, f3, imm, rs2, rs1;
-} b_type;
-
-typedef struct j_type{
-    char inst_name[7];
-    int opcode, imm, rd;
-} j_type;
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include "riscv-inst-read.h"
 
 char byteToHex(uint8_t byte);
 
@@ -70,6 +45,136 @@ void RtypeToHex(r_type instStruct, char * instruction){
     instruction[0] = byteToHex(byte7);
     instruction[8] = '\0';
     //uint32_t instruction = (byte7 << 28) + (byte6 << 24) + (byte5 << 20) + (byte4 << 16) + (byte3 << 12) + (byte2 << 8) + (byte1 << 4) + byte0;
+}
+
+void ItypeToHex(i_type instStruct, char * instruction){
+    uint8_t byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7, temp;
+    uint8_t temp1, temp2;
+    uint16_t temp3;
+
+    temp1 = instStruct.opcode << 4;
+    byte0 = temp1 >> 4;
+
+    temp1 = instStruct.rd << 7;
+    temp1 = temp1 >> 4;
+    temp2 = instStruct.opcode >> 4;
+    byte1 = temp1 + temp2;
+
+    byte2 = instStruct.rd >> 1;
+
+    temp1 = instStruct.rs1 << 7;
+    temp1 = temp1 >> 4;
+    byte3 = instStruct.f3 + temp1;
+
+    byte4 = instStruct.rs1 >>1;
+
+    temp3 = instStruct.imm << 8;
+    byte5 = temp3 >> 8;
+
+    temp3 = instStruct.imm << 4;
+    byte6 = temp3 >> 8;
+    
+    byte7 = instStruct.imm >> 8;
+
+    instruction[7] = byteToHex(byte0);
+    instruction[6] = byteToHex(byte1);
+    instruction[5] = byteToHex(byte2);
+    instruction[4] = byteToHex(byte3);
+    instruction[3] = byteToHex(byte4);
+    instruction[2] = byteToHex(byte5);
+    instruction[1] = byteToHex(byte6);
+    instruction[0] = byteToHex(byte7);
+    instruction[8] = '\0';
+}
+
+void StypeToHex(s_type instStruct, char * instruction){
+    uint8_t byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7, temp;
+    uint8_t temp1, temp2;
+    int imm4_0 = (instStruct.imm << 7) >> 7;
+    int imm11_5 = instStruct.imm >> 5;
+
+    temp1 = instStruct.opcode << 4;
+    byte0 = temp1 >> 4;
+
+    temp1 = imm4_0 << 7;
+    temp1 = temp1 >> 4;
+    temp2 = instStruct.opcode >> 4;
+    byte1 = temp1 + temp2;
+
+    byte2 = imm4_0 >> 1;
+
+    temp1 = instStruct.rs1 << 7;
+    temp1 = temp1 >> 4;
+    byte3 = instStruct.f3 + temp1;
+
+    byte4 = instStruct.rs1 >>1;
+
+    temp1 = instStruct.rs2 << 4;
+    byte5 = temp1 >> 4;
+
+    temp1 = imm11_5 << 4;
+    temp1 = temp1 >> 4;
+    temp2 = instStruct.rs2 >> 4;
+    byte6 = temp1 + temp2;
+    
+    byte7 = imm11_5 >> 3;
+
+    instruction[7] = byteToHex(byte0);
+    instruction[6] = byteToHex(byte1);
+    instruction[5] = byteToHex(byte2);
+    instruction[4] = byteToHex(byte3);
+    instruction[3] = byteToHex(byte4);
+    instruction[2] = byteToHex(byte5);
+    instruction[1] = byteToHex(byte6);
+    instruction[0] = byteToHex(byte7);
+    instruction[8] = '\0';
+}
+
+void BtypeToHex(b_type instStruct, char * instruction){
+    uint8_t byte0, byte1, byte2, byte3, byte4, byte5, byte6, byte7, temp;
+    uint8_t temp1, temp2;
+
+    temp1 = (instStruct.imm << 1) >> 10;
+    int imm4_1_11 = ((instStruct.imm << 9) >> 8) + temp1;
+    
+    temp1 = (instStruct.imm >> 11) << 7;
+    int imm12_10_5 = (instStruct.imm >> 3) + temp1;
+
+    temp1 = instStruct.opcode << 4;
+    byte0 = temp1 >> 4;
+
+    temp1 = imm4_1_11 << 7;
+    temp1 = temp1 >> 4;
+    temp2 = instStruct.opcode >> 4;
+    byte1 = temp1 + temp2;
+
+    byte2 = imm4_1_11 >> 1;
+
+    temp1 = instStruct.rs1 << 7;
+    temp1 = temp1 >> 4;
+    byte3 = instStruct.f3 + temp1;
+
+    byte4 = instStruct.rs1 >>1;
+
+    temp1 = instStruct.rs2 << 4;
+    byte5 = temp1 >> 4;
+
+    temp1 = imm12_10_5 << 4;
+    temp1 = temp1 >> 4;
+    temp2 = instStruct.rs2 >> 4;
+    byte6 = temp1 + temp2;
+    
+    byte7 = imm12_10_5 >> 3;
+
+    instruction[7] = byteToHex(byte0);
+    instruction[6] = byteToHex(byte1);
+    instruction[5] = byteToHex(byte2);
+    instruction[4] = byteToHex(byte3);
+    instruction[3] = byteToHex(byte4);
+    instruction[2] = byteToHex(byte5);
+    instruction[1] = byteToHex(byte6);
+    instruction[0] = byteToHex(byte7);
+    instruction[8] = '\0';
 }
 
 char byteToHex(uint8_t byte){
