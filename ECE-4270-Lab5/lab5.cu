cb95205d56978a59fcf59c99648612ab32326a27
ashbtp <ashbtp@cengr-c1251-114.engineering.missouri.edu>
2023-04-20 16:48:17 -0500

    added lab5.cu file

---
+++ ECE-4270-Lab5/lab5.cu
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+__global__ void square(float *d_out, float *d_a, float * d_b){
+	int idx = threadIdx.x;
+	float a = d_a[idx];
+	float b = d_b[idx];
+	d_out[idx] = a + b;
+}
+
+int main(int argc, char ** argv){
+	const int ARRAY_SIZE = 4;
+	const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
+	
+	float h_in[ARRAY_SIZE];
+	for (int i=0; i < ARRAY_SIZE; i++){
+		h_in[i] = float(i);
+	}
+    float h_in2[ARRAY_SIZE];
+	for (int i=0; i < ARRAY_SIZE; i++){
+		h_in2[i] = float(i + 4);
+	}
+	float h_out[ARRAY_SIZE];
+    printf("Input matrix 1:\n");
+    for (int i =0; i<ARRAY_SIZE; i++){
+		printf("%.2f", h_in[i]);
+		printf(((i % 2) !=1)? "\t": "\n");
+	}
+    printf("\nInput matrix 2:\n");
+    for (int i =0; i<ARRAY_SIZE; i++){
+		printf("%.2f", h_in2[i]);
+		printf(((i % 2) !=1)? "\t": "\n");
+	}
+
+	float *d_in;
+	float *d_in2;
+	float *d_out;
+
+	cudaMalloc((void**) &d_in, ARRAY_BYTES); 
+	cudaMalloc((void**) &d_in2, ARRAY_BYTES); 
+	cudaMalloc((void**) &d_out, ARRAY_BYTES); 
+
+	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_in2, h_in2, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+	square<<<1, ARRAY_SIZE>>>(d_out, d_in, d_in2);
+
+	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+
+    printf("\nOutput matrix:\n");
+	for (int i =0; i<ARRAY_SIZE; i++){
+		printf("%.2f", h_out[i]);
+		printf(((i % 2) !=1)? "\t": "\n");
+	}
+	
+	cudaFree(d_in);
+	cudaFree(d_in2);
+	cudaFree(d_out);
+
+	return 0;
+}
